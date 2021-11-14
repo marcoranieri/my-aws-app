b8558ba146fbf83959784b2a7cec162874da4712
marco ranieri <mr.marcoranieri@gmail.com>
2021-11-14 11:48:23 +0000

    INIT

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Article.destroy_all
+User.destroy_all
+
+user = User.create! email: "test@test.com", password: "123456"
+
+Article.create! title: "my title ONE", body: "this is my boooooody", user: user
+Article.create! title: "my title TWO", body: "this is my booooooooooody", user: user
+Article.create! title: "my title THREE", body: "this is my boody", user: user
+
+puts "Created #{Article.count} Articles"
